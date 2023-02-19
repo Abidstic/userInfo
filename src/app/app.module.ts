5136d5393faa0540342b0f40caea65ea04d328e7
MahinAbid <abid.mahmud@orbitax.com>
2023-02-19 18:11:33 +0600

    changed styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { UserGridComponent } from './user-grid/user-grid.component';
 import { UserFormComponent } from './user-form/user-form.component';
+import { UserInformationComponent } from './user-information/user-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserGridComponent,
-    UserFormComponent
+    UserFormComponent,
+    UserInformationComponent
   ],
   imports: [
     BrowserModule
